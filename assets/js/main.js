e350907b3c660c2936ba68d8e1efa7a2f9f0a047
Daniel Carl Jones <daniel.carl@live.com>
2016-07-23 14:33:38 +0100

    Add smooth scrolling and offset to anchor links.

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,6 +165,20 @@
 
 				});
 
+        // Smooth Scroll
+        $('a.smooth[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top - 40
+              }, 1000);
+              return false;
+            }
+          }
+        });
+
 	});
 
 })(jQuery);
