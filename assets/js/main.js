382ee4d7d451d96f990d8a675899353cf75f6d58
Daniel Carl Jones <daniel.carl@live.com>
2016-08-05 12:58:34 +0100

    Remove MP4 (too big), remove video from mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,4 +181,9 @@
 
 	});
 
+  var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+  if (mobile) {
+      $('video.fullscreen').css('display', 'none'); // OR you can use $('.navWrap').hide();
+  }
+
 })(jQuery);
